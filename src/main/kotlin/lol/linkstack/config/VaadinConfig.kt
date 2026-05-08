1a8d02ab1a8ada90e0348d3708ba736d5b033b12
Nijasc <github.moonshine694@passmail.net>
2026-05-08 08:29:22 +0200

    Switched from Lumo to Aura theme

--- src/main/kotlin/lol/linkstack/config/VaadinConfig.kt
+++ src/main/kotlin/lol/linkstack/config/VaadinConfig.kt
@@ -4,9 +4,10 @@ import com.vaadin.flow.component.dependency.StyleSheet
 import com.vaadin.flow.component.page.AppShellConfigurator
 import com.vaadin.flow.component.page.ColorScheme
 import com.vaadin.flow.component.page.Viewport
+import com.vaadin.flow.theme.aura.Aura
 import com.vaadin.flow.theme.lumo.Lumo
 
-@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Aura.STYLESHEET)
 @ColorScheme(ColorScheme.Value.DARK)
 @Viewport("width=device-width, initial-scale=1.0, viewport-fit=cover")
 class VaadinConfig : AppShellConfigurator
